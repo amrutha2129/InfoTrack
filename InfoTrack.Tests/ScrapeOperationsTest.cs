9f0f26f4bdfbc62788aca727bf4573494a37abf5
amrutha2129 <s.vas.amrutha@gmail.com>
2019-04-16 16:26:25 +1000

    Added the Unit Test Project in the solution

---
+++ InfoTrack.Tests/ScrapeOperationsTest.cs
@@ -0,0 +1,26 @@
+﻿using InfoTrack.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfoTrack.Tests
+{
+    [TestFixture]
+    class ScrapeOperationsTest
+    {
+        private readonly ScrapeOperations _scrapeOperations;
+
+        public ScrapeOperationsTest()
+        {
+            _scrapeOperations = new ScrapeOperations();
+        }
+
+        [Test]
+        public void checkInfoTracklRanking()
+        {
+            var result = _scrapeOperations.parseResultPageAsync("online title search", "https://www.infotrack.com.au");
+            //Assert.AreEqual(result, {rankList=[0]});
+        }
+   }
+}
--- InfoTrack/Controllers/TitleSearchController.cs
+++ InfoTrack/Controllers/TitleSearchController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using InfoTrack.Models;
 using InfoTrack.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -26,52 +21,6 @@ namespace InfoTrack.Controllers
             
             return await _scrapeOperations.parseResultPageAsync(searchWord, searchUrl);
         }
-
-        //public UrlRank parseResultPage(string resultPage, string searchUrl)
-        //{
-        //    Dictionary<int,string> urlRankMap = new Dictionary<int, string>();
-        //    int i = 0, k;
-        //    var startOfResults = findStartOfResults(resultPage);
-        //    var resultSections = startOfResults.Split("<div class=\"g\">");
-        //    for (k = 1; k < resultSections.Length; k++)
-        //    {
-        //        urlRankMap.Add(i++,getUrlFromSections(resultSections[k]));
-        //    }
-        //    return getRankList(urlRankMap, searchUrl);
-        //}
-        //private string findStartOfResults(string resultPage)
-        //{
-        //    string[] authorsList = resultPage.Split("ires");
-        //    return authorsList[1];
-        //}
-
-        //private string getUrlFromSections(string section)
-        //{
-        //    int startOfUrl = section.IndexOf("http");
-        //    if(section[startOfUrl+4] == 's')
-        //    {
-        //        return section.Substring(startOfUrl, (section.IndexOf("/", startOfUrl + 8)) - startOfUrl);
-        //    }
-        //    else
-        //    {
-        //        return section.Substring(startOfUrl, (section.IndexOf("/", startOfUrl + 7)) - startOfUrl);
-        //    }
-            
-        //}
-
-        //private UrlRank getRankList(Dictionary<int, string> urlRankMap, string searchUrl)
-        //{
-        //    UrlRank urlRank = new UrlRank();
-        //    foreach (KeyValuePair<int, string> item in urlRankMap)
-        //    {
-        //        if (item.Value.Equals(searchUrl))
-        //        {
-        //            urlRank.RankList.Add(item.Key);
-        //        }
-
-        //    }
-        //    return urlRank;
-        //}
-
+       
     }
 }
